63f51ec1dc34243640beb5481aaa044296f98424
Rebegea Dragos-Alexnadru <dragosalex97@gmail.com>
2021-04-14 22:46:35 +0300

    [TS bot] New details for agency info

--- utils.py
+++ utils.py
@@ -2,18 +2,37 @@ MainMenu, AgencyInfo, RedelegationPerion, SubscriptionsMenu, availableSpace = ra
 
 from erdpy.contracts import SmartContract
 from erdpy.proxy import ElrondProxy
+import emoji
 mainnet_proxy = ElrondProxy('https://gateway.elrond.com')
 TrustStaking_contract = SmartContract('erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqzhllllsp9wvyl')
-
+db_token = '1724076081:AAFhcHoFcmT8tNl90Gge7mbYZn-1cunu7j4'
+bot_token = '1724076081:AAFhcHoFcmT8tNl90Gge7mbYZn-1cunu7j4'
 
 agency_info = '''
-<code>Agency: </code><a href="http://truststaking.com/">Trust Staking {}</a>
+<code>Agency: </code><a href="http://truststaking.com/">Trust Staking''' + emoji.thunder + '''</a>
 <code>Contract Address: </code>{}
 <code>Service fee: </code>{}% {}
 <code>Max delegation cap: </code>{:.0f} eGLD ({:.2f}% filled)
+<code>Nodes: </code>{} <code>active</code> +{} <code>staked</code>
+<code>Eligible today: </code>{}
 <code>Delegators: </code>{}
 <code>Total active stake: </code>{:.2f} eGLD
-<code>Available: </code>{:.2f} eGLD
+<code>Available: </code>{} eGLD
+<code>Top-up per node: </code>{:.2f} eGLD
+<code>APR: </code>{}
+'''
+
+extra = '''
+<code>Eligible:</code> 
+    ''' + emoji.full_moon + '''<code>Online:</code> {} ''' + emoji.new_moon + '''<code>Offline:</code> {}
+<code>Waiting:</code>
+    ''' + emoji.full_moon + '''<code>Online:</code> {} ''' + emoji.new_moon + '''<code>Offline:</code> {}
+<code>New:</code>
+    ''' + emoji.full_moon + '''<code>Online:</code> {} ''' + emoji.new_moon + '''<code>Offline:</code> {}
+<code>Queued:</code>
+    ''' + emoji.full_moon + '''<code>Online:</code> {} ''' + emoji.new_moon + '''<code>Offline:</code> {}
+<code>Jailed:</code>
+    ''' + emoji.full_moon + '''<code>Online:</code> {} ''' + emoji.new_moon + '''<code>Offline:</code> {}
 '''
 # <code>Nodes: </code>{}
 # <code>Top-up per node: </code>{} eGLD
